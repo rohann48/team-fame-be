be7222cc2aed639e14731f5af5cc609b7f526aae
rohan <rohann48@gmail.com>
2024-12-08 18:23:12 +0530

    certs

--- src/app.ts
+++ src/app.ts
@@ -33,15 +33,15 @@ const razorpay = new Razorpay({
 
 // Certificate
 const privateKey = fs.readFileSync(
-  "/etc/letsencrypt/live/api.team-fame.com/privkey.pem",
+  "/etc/letsencrypt/live/team-fame.com/privkey.pem",
   "utf8"
 );
 const certificate = fs.readFileSync(
-  "/etc/letsencrypt/live/api.team-fame.com/cert.pem",
+  "/etc/letsencrypt/live/team-fame.com/cert.pem",
   "utf8"
 );
 const ca = fs.readFileSync(
-  "/etc/letsencrypt/live/api.team-fame.com/chain.pem",
+  "/etc/letsencrypt/live/team-fame.com/chain.pem",
   "utf8"
 );
 
